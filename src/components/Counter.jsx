08a5fc4508adaeaeb4651c11c8501f89f8070c5b
sandra7968 <sandrapylee@gmail.com>
2023-11-05 18:07:14 +0530

    counter-app

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { decrement, increment, reset } from '../redux/counterSlice'
+import { decrement, increment, incrementByAmount, reset } from '../redux/counterSlice'
 
 function Counter() {
+    const [amount,setAmount] = useState("")
     const dispatch = useDispatch()
    const count =  useSelector((state)=>state.counter.count)
+   const handleIncrement = ()=>{
+    if(amount==""){
+        alert("Please provide amount")
+    }else{
+        // update state in store
+        dispatch(incrementByAmount(Number(amount)))
+    }
+   }
   return (
     <div style={{height:'70vh'}} className='w-100 d-flex justify-content-center align-items-center'>
         <div className='d-flex justify-content-center align-items-center border rounded p-5 w-25 flex-column'>
@@ -13,6 +22,11 @@ function Counter() {
                 <button onClick={()=>dispatch(decrement())} className='btn btn-warning'>Decrement</button>
                 <button onClick={()=>dispatch(reset())} className='btn btn-danger'>Reset</button>
                 <button onClick={()=>dispatch(increment())} className='btn btn-success'>Increment</button>
+
+            </div>
+            <div className="d-flex mt-5">
+                <input type="text" className='form-control' placeholder='Enter the amount to be incremented' onChange={e=>setAmount(e.target.value)} />
+                <button onClick={handleIncrement} className='btn btn-primary ms-3'>Increment by Amount</button>
 
             </div>
         </div>
